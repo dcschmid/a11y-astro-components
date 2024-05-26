8cf6d077eef69c0a1067ec8747f5505d65023353
dcschmid <dcschmid@murena.io>
2024-05-26 21:03:52 +0200

    feat: add skipLink component

--- index.ts
+++ index.ts
@@ -1,6 +1,4 @@
 // Do not write code directly here, instead use the `src` folder!
 // Then, use this file to export everything you want your user to access.
 
-import MyComponent from './src/MyComponent.astro';
-
-export default MyComponent;
+export { default as SkipLinks } from './src/SkipLinks.astro';
